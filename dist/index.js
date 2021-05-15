05a11afdfa426a1ea171fd948156166792b323e5
Miguel Casqueira <miguel@balena.io>
2021-05-14 21:40:55 -0400

    ugh
    Signed-off-by: Miguel Casqueira <miguel@balena.io>

--- dist/index.js
+++ dist/index.js
@@ -6059,11 +6059,7 @@ const core = __nccwpck_require__(800);
 const github = __nccwpck_require__(996);
 
 try {
-  // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput('who-to-greet');
-  console.log(`Hello ${nameToGreet}!`);
-  const time = (new Date()).toTimeString();
-  core.setOutput("time", time);
+  console.log(`I need to find the releaseID from the PR which triggered this.`);
   // Get the JSON webhook payload for the event that triggered the workflow
   const payload = JSON.stringify(github.context.payload, undefined, 2)
   console.log(`The event payload: ${payload}`);
