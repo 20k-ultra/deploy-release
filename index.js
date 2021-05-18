e20ac663f970a4a62d482f7dda4fbb01992de6b2
Miguel Casqueira <miguel@balena.io>
2021-05-18 16:07:28 -0400

    added additiona logging
    Signed-off-by: Miguel Casqueira <miguel@balena.io>

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 
 try {
+  console.log('I am running because a PR was closed and merged into master.')
   console.log(`I need to find the releaseID from the PR which triggered this.`);
   // Get the JSON webhook payload for the event that triggered the workflow
   const payload = JSON.stringify(github.context.payload, undefined, 2)
